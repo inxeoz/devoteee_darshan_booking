f66b7c8b69e5ddbc389c8ec45e529433cb7be5f1
inxeoz <inxeoz@inxeoz.com>
2025-10-13 12:46:44 +0530

    time and date

--- src/routes/ShowAppointment.svelte
+++ src/routes/ShowAppointment.svelte
@@ -108,8 +108,13 @@
             </div>
             <div><strong>Type:</strong> {data.darshan_type ?? "—"}</div>
             <div>
-                <strong>Date & Time:</strong>
-                {formatDateTime(data.darshan_date, data.darshan_time)}
+                <strong>Date:</strong>
+                {data.darshan_date}
+            </div>
+
+            <div>
+                <strong>Time:</strong>
+                {data.slot_start_time} to {data.slot_end_time}
             </div>
             <div>
                 <strong>With Protocol:</strong>
