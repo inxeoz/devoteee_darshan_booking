51f43f6473563ceb1b19c2b788360470781af34b
inxeoz <inxeoz@inxeoz.com>
2025-11-11 23:40:02 +0530

    backend port 8001

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,8 +1,8 @@
-
-
-export const env = typeof window !== 'undefined' ? window.__ENV__ || {} : {};
+export const env = typeof window !== "undefined" ? window.__ENV__ || {} : {};
 export const isProd = import.meta.env.PROD;
 export const API_BASE = env.API_BASE || import.meta.env.VITE_TARGET;
 
-if (typeof window !== 'undefined')
-  console.log(`Mode: ${isProd ? 'prod' : 'dev'} | API_BASE=${API_BASE} <--- window`);
+if (typeof window !== "undefined")
+  console.log(
+    `Mode: ${isProd ? "prod" : "dev"} | API_BASE=${API_BASE} <--- window`,
+  );
--- static/env-config.js
+++ static/env-config.js
@@ -1,3 +1,3 @@
 window.__ENV__ = {
-  API_BASE: "default-api-url" // this gets replaced at container startup
+  API_BASE: "http://127.0.0.1:8001", // this gets replaced at container startup
 };
